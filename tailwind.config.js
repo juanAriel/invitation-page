0905c9f1d136d0341a824e07d32d2aabfc2a10bc
juanAriel <arielvia2013@gmail.com>
2025-11-11 10:58:57 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pacay: '#b8d5a4',
+        gold: '#d4af37',
+        navy: '#0a2540',
+      },
+    },
+  },
+  plugins: [],
+}
